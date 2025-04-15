8f8538eb1a026b6a8b4170d722b69b1fe0f21153
alungggggg <aldinoalungputraanugraha@gmail.com>
2025-04-15 14:36:38 +0700

    scoring

---
+++ app/Http/Controllers/ScoreController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ScoreModel;
+use Illuminate\Http\Request;
+
+class ScoreController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        try {
+            if($request->id) {
+                $data = ScoreModel::find($request->id);
+                if(!$data) {
+                    return response()->json([
+                        'status' => false,
+                        'message' => 'Score not found'
+                    ], 404);
+                }
+                return response()->json([
+                    'status' => true,
+                    'data' => $data,
+                ]);
+            }
+            return response()->json([
+                'status' => true,
+                'data' => ScoreModel::all()
+            ]);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'status' => false,
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $score = new ScoreModel();
+            $score->uuid = $request->uuid;
+            $score->id_user = $request->id_user;
+            $score->score = $request->score;
+            $score->status = $request->status;
+            $score->save();
+
+            return response()->json([
+                'status' => true,
+                'data' => $score
+            ]);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'status' => false,
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function destroy(Request $request)
+    {
+        try {
+            $score = ScoreModel::find($request->id);
+            if(!$score) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Score not found'
+                ], 404);
+            }
+            $score->delete();
+            return response()->json([
+                'status' => true,
+                'message' => 'Score deleted'
+            ]);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'status' => false,
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function edit(Request $request){
+        try {
+            $score = ScoreModel::find($request->id);
+            if(!$score) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Score not found'
+                ], 404);
+            }
+            $score->uuid = $request->uuid;
+            $score->id_user = $request->id_user;
+            $score->score = $request->score;
+            $score->status = $request->status;
+            $score->save();
+
+            return response()->json([
+                'status' => true,
+                'data' => $score
+            ]);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'status' => false,
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+}
---
+++ app/Models/ScoreModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ScoreModel extends Model
+{
+    use HasFactory;
+    protected $table = 'score';
+    protected $primaryKey = 'uuid';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    public $timestamps = false;
+
+    protected $fillable = [
+        'uuid',
+        'id_user',
+        'score',
+        'status',
+    ];
+}
